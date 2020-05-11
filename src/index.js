b6e378f87670bb247955855c31e4eb088ac64e77
Yavuzgrdl <32365733+Yavuzgrdl@users.noreply.github.com>
2020-05-11 11:13:30 +0200

    aa

--- src/index.js
+++ src/index.js
@@ -4,10 +4,15 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { BrowserRouter as Router } from 'react-router-dom'
+
 ReactDOM.render(
-  <React.StrictMode>
+  //<React.StrictMode>
+  <Router>
     <App />
-  </React.StrictMode>,
+  </Router>
+  //</React.StrictMode>
+  ,
   document.getElementById('root')
 );
 
---
+++ src/pages/Deneme.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Deneme() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Deneme
